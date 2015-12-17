3774acba23c6f257e5e52ebf3db16a173637fb5a
Alex Golub <alexeythumb@gmail.com>
2015-12-17 18:36:26 +0200

    MainViewController UI preconfigured

--- CDVDictionary/CDVDictionary/MainViewController.swift
+++ CDVDictionary/CDVDictionary/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  CDVDictionary
 //
 //  Created by Alex Golub on 12/17/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
