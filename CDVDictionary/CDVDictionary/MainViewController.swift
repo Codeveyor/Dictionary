cfe2a436c6bf0fd3a0738a1280185ac6a6c1f0be
Alex Golub <alexeythumb@gmail.com>
2015-12-17 19:05:20 +0200

    MainViewController main methods preset

--- CDVDictionary/CDVDictionary/MainViewController.swift
+++ CDVDictionary/CDVDictionary/MainViewController.swift
@@ -8,18 +8,66 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var displayArray = ["a", "b", "c"]
+    
+    let textCellIdentifier = "mainTableCellidentifier"
+    let tableViewNumberOfSections:Int = 1
+    let tableViewCellHeight: CGFloat = 50.0
+    let tableViewHeaderHeight: CGFloat = 0.01
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+ 
+    // MARK: TableView DataSource
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return tableViewNumberOfSections
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return displayArray.count
     }
-
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel?.text = displayArray[indexPath.row]
+        cell.detailTextLabel?.text = displayArray[indexPath.row]
+        
+        return cell
+    }
+    
+    // MARK: TableView Delegate
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return tableViewCellHeight
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return tableViewHeaderHeight
+    }
+    
+    // MARK: SearchBar Delegate
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
 }
 
