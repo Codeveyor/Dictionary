9abdc68132a31eed62878da7067a04ff841a25e9
Alex Golub <alexeythumb@gmail.com>
2015-12-17 18:29:01 +0200

    Added project

---
+++ CDVDictionary/CDVDictionary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CDVDictionary
+//
+//  Created by Alex Golub on 12/17/15.
+//  Copyright © 2015 Alex Golub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CDVDictionary/CDVDictionary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CDVDictionary
+//
+//  Created by Alex Golub on 12/17/15.
+//  Copyright © 2015 Alex Golub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
