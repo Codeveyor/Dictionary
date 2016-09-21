e0376350f45f676e8148a4da721902504e207fcb
Alex Golub <alexeythumb@gmail.com>
2016-09-21 14:49:43 +0300

    Remove unused code from AppDelegate

--- CDVDictionary/CDVDictionary/AppDelegate.swift
+++ CDVDictionary/CDVDictionary/AppDelegate.swift
@@ -13,45 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions
                    launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        copyPlistFiles()
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-    }
-    
-    func copyPlistFiles() {
-//        
-//        let bundlePath = NSBundle.mainBundle().pathForResource("russianAlphabet", ofType: ".plist")
-////        print(bundlePath, "\n") //prints the correct path
-//        let destinationPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true).first!
-//        let fileManager = NSFileManager.defaultManager()
-//        let fullDestinationPath = NSURL(fileURLWithPath: destinationPath).URLByAppendingPathComponent("russianAlphabet.plist")
-//        let fullDestinationPathString = fullDestinationPath.path
-////        print(fileManager.fileExistsAtPath(bundlePath!)) // prints true
-//
-//        do {
-//            try fileManager.copyItemAtPath(bundlePath!, toPath: fullDestinationPathString!)
-//        } catch {
-//            print("\n")
-//            print(error)
-//        }
-    }
 }
-
-
