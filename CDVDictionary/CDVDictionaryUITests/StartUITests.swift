e61e3f9f89bd9acdce2fc6c0465a1a7b101f252a
Alex Golub <alexeythumb@gmail.com>
2016-10-03 10:23:47 +0300

    Preset UITests

---
+++ CDVDictionary/CDVDictionaryUITests/StartUITests.swift
@@ -0,0 +1,26 @@
+//
+//  StartUITests.swift
+//  StartUITests
+//
+//  Created by Alex Golub on 2/1/16.
+//  Copyright © 2016 Alex Golub. All rights reserved.
+//
+
+import XCTest
+
+class StartUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testSwitchSegments() {
+
+    }
+}
