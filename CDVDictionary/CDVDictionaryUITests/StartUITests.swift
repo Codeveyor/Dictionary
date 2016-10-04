3678c13e4a195e6828fbcd86d8d0c9d332f484e7
Alex Golub <alexeythumb@gmail.com>
2016-10-05 00:02:02 +0300

    Add first UI test

--- CDVDictionary/CDVDictionaryUITests/StartUITests.swift
+++ CDVDictionary/CDVDictionaryUITests/StartUITests.swift
@@ -9,17 +9,28 @@
 import XCTest
 
 class StartUITests: XCTestCase {
+    fileprivate let application = XCUIApplication()
     override func setUp() {
         super.setUp()
         continueAfterFailure = false
-        XCUIApplication().launch()
-    }
-
-    override func tearDown() {
-        super.tearDown()
+        application.launch()
     }
 
     func testSwitchSegments() {
-        
+        let rsLabel = "Р→С"
+        let srLabel = "S→R"
+        let iLabel = "ℹ︎"
+
+        let cdvdictionaryMainviewNavigationBar = application.navigationBars["CDVDictionary.MainView"]
+        let srSegment = cdvdictionaryMainviewNavigationBar.segmentedControls.buttons[srLabel]
+        srSegment.tap()
+        let iSegment = cdvdictionaryMainviewNavigationBar.segmentedControls.buttons[iLabel]
+        iSegment.tap()
+        let rsSegment = cdvdictionaryMainviewNavigationBar.segmentedControls.buttons[rsLabel]
+        rsSegment.tap()
+
+        XCTAssertEqual(rsLabel, rsSegment.label, "rs segment label should match")
+        XCTAssertEqual(srLabel, srSegment.label, "sr segment label should match")
+        XCTAssertEqual(iLabel, iSegment.label, "i segment label should match")
     }
 }
